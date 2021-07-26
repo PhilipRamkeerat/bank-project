29e84b9c87edf14d5744836c46b130651159ca54
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2021-07-25 20:59:37 -0400

    Add CUSTOM_ELEMENTS_SCHEMA on app module to allow custom tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NewTransferComponent } from './new-transfer/new-transfer.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [NewTransferComponent]
+  exports: [NewTransferComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
